02f0c703bd5b3747ee54e35bd0a7fa9d75d2c74f
Bahtiyor1 <bakhtierbek2011@gmail.com>
2024-09-16 17:38:07 +0500

    math

---
+++ script.js
@@ -0,0 +1,30 @@
+// let num1 = prompt('Number1')
+// let num2 = prompt('Number2')
+// let num3 = prompt('Number3')
+
+// alert(Math.max(num1,num2,num3))
+
+//let num = prompt('Random Number')
+
+//alert(Math.round(Math.random() * num))
+
+
+
+
+
+
+alert('Sizda hozir random sonlar chiqishni boshlaydi');
+
+var randomNumber = Math.floor(Math.random() * 100); 
+alert('Random orqali chiqqan son: ' + randomNumber);
+
+result = confirm('Biz random sonni 2 ga ko\'paytirib va bo\'lmoqchimiz!');
+
+alert('Random orqali chiqqan son: ' + randomNumber + 'edi.\n' +
+      'Biz uni 2 ga ko\'paytirib ' + (randomNumber * 2) + ' sonini chiqardik.\n' +
+      'Va shu sonni 2 ga bo\'lib: ' + (randomNumber * 1 / 2) + ' sonini chiqardik.\n' +
+      'Va shu sonni 3 ga bolgandagi: ' + (randomNumber * 2 / 3 ) + ' sonini chiqardik.');
+
+
+
+
